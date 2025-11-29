3b1db6558b0e3933650802464641f56441f3a8cd
Milana Kovacevic <miki@192.168.1.6>
2025-11-29 17:54:31 +0100

    added js consts

---
+++ script.js
@@ -0,0 +1,8 @@
+const balanceEl = document.getElementById('balance');
+const incomeAmountEl = document.getElementById('income-amount');
+const expenseAmountEl = document.getElementById('expense-amount');
+const transactionListEl = document.getElementById('transaction-list');
+const transactionFormEl = document.getElementById('transacton-form');
+const descriptionEl = document.getElementById('description');
+const amountEl = document.getElementById('amount');
+
